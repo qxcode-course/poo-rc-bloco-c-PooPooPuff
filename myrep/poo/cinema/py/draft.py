2df230e3a0c58649d24d21c18d133fb25bc6e5e0
Luis <luiscarlosmaiasilva@alu.ufc.br>
2025-11-19 23:49:34 -0300

    naoseioqfizmasocinemafunfou

--- myrep/poo/cinema/py/draft.py
+++ myrep/poo/cinema/py/draft.py
@@ -15,14 +15,18 @@ class Client:
     def setphone(self,phone:int):
         self.__phone=phone
     
-    #def __str__(self):
-        
+    def __str__(self):
+        return f"{self.__id}:{self.__phone}"
     
-#class Theather:
-    #def __init__(self,capacity:int):
-        #self.__seats:list[]
+class Theather:
+    def __init__(self,capacity:int):
+        self.__seats:list[Client|None]=[None]*capacity
+        
+    def __str__(self):
+        return "["+" ".join([str(x) if x else "-" for x in self.__seats])+"]"
+    
 def main():
-    #theather=Theather()
+    theather=Theather(0)
     while True:
         line=input()
         print("$"+line)
@@ -30,6 +34,9 @@ def main():
         
         if args[0]=="end":
             break
-        #elif args[0]=="show":
-            #print(theather)
+        elif args[0]=="show":
+            print(theather)
+        elif args[0]=="init":
+            capacity=int(args[1])
+            theather=Theather(capacity)
 main()
